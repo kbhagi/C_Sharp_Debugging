f22fe768929a2bf614ed4ff15d1c7b05f1858dbc
bhargavak <bhargavak37@gmail.com>
2017-08-27 11:10:51 +0530

    Logic changes
    Logic Changes

--- Program.cs
+++ Program.cs
@@ -1,35 +1,41 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace CSharpFundamentals
+namespace C_Sharp_Fundamentals
 {
-class Program
-{
-public static void Main(string[] args)
-{
-var numbers = new List<int>{1,2,3,4,5,6};//declare the List
-var smallests=GetSmallests(numbers,3);
-foreach(var number in smallests) //Iterate over the List & display the numbers
-  Console.WriteLine(number);
-  }
-  public static List<int> GetSmallests(List<int> list, int count)
-  {
- var smallests = new List<int>();
- while (smallests.Count < count)
- {
- var min=GetSmallest(list);
- smallests.Add(min);
- list.Remove(min);
- }
-  }
-  public static int GetSmallest(List<int> list)
-  {
-  //Assume the first number is the smallest
-  var min=list[0];
-  for(var i=1;i<list.Count;i++)
-  {
-  if (list[i] < min)
-      min = list[i];
-  }
-  return min;
-  }
+    class Find3SmallestNumbers
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = new List<int> { 1, 2, 3, 4, 5, 6 };//declare the List
+            var smallests = GetSmallests(numbers, 3);
+            foreach (var number in smallests) //Iterate over the List & display the numbers
+                Console.WriteLine(number);
+        }
+        public static List<int> GetSmallests(List<int> list, int count)
+        {
+            var smallests = new List<int>();
+            while (smallests.Count < count)
+            {
+                var min = GetSmallest(list);
+                smallests.Add(min);
+                list.Remove(min);
+            }
+            return smallests;
+        }
+        public static int GetSmallest(List<int> list)
+        {
+            //Assume the first number is the smallest
+            var min = list[0];
+            for (var i = 1; i < list.Count; i++)
+            {
+                if (list[i] < min)
+                    min = list[i];
+            }
+            return min;
+        }
+    }
+}
